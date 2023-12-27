109314f9184b15db882902985180aad618303ff6
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-27 20:47:45 +0200

    Chatbot 2st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -6,6 +6,10 @@ def main():
     print("Hello! My name is DICT_Bot.")
     print("I was created in 2023.")
 
+    # Етап 2
+    name = input("Please, remind me your name.\n> ")
+    print(f"What a great name you have, {name}!")
+
 
 if __name__ == "__main__":
     main()
