651a5e396b9f133d0d174d0792202fed42860783
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-27 20:50:13 +0200

    Chatbot 3st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -10,6 +10,12 @@ def main():
     name = input("Please, remind me your name.\n> ")
     print(f"What a great name you have, {name}!")
 
+    # Етап 3
+    remainder3 = int(input("Enter remainder of dividing your age by 3.\n> "))
+    remainder5 = int(input("Enter remainder of dividing your age by 5.\n> "))
+    remainder7 = int(input("Enter remainder of dividing your age by 7.\n> "))
+    age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    print(f"Your age is {age}; that's a good time to start programming!")
 
 if __name__ == "__main__":
     main()
